2f4de14283f41729e9e1ba850e4545ddd8ed4177
raihannuryasintcn <raihannuryasintcn@gmail.com>
2025-02-13 22:43:24 +0700

    added lottie error animation in 404 error page

--- src/pages/_404.jsx
+++ src/pages/_404.jsx
@@ -1,6 +1,13 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
 export function NotFound() {
 	return (
 		<section>
+			<DotLottieReact
+			src="https://lottie.host/1783740f-9bc6-4c6f-b175-79961758cb13/Om0gxrXJRO.lottie"
+			loop
+			autoplay
+			/>
 			<h1>404: Not Found</h1>
 			<p>It's gone :(</p>
 		</section>
